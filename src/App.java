79708d1775b6778ec9dc7f3554c769a8b580f7ab
Bharani-o7 <145083138+Bharani-o7@users.noreply.github.com>
2023-09-14 19:49:27 -0700

    first commit

---
+++ src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String account = "Prime";
+        double total = 210.35;
+        int years = 5;
+        
+        System.out.printf("The %s account saved you $%.1f over %d years\n",
+        account, total, years);
+    }
+}
